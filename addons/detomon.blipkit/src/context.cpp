d44f75f08238ff130119d2868e7ea299bf4d8d38
detomon <simon@monoxid.ch>
2024-05-01 09:53:22 +0200

    Initial commit

---
+++ addons/detomon.blipkit/src/context.cpp
@@ -0,0 +1,16 @@
+#include <godot_cpp/core/math.hpp>
+
+#include "context.hpp"
+
+using namespace detomon::BlipKit;
+using namespace godot;
+
+Context::Context() {
+	// ...
+}
+
+void Context::_bind_methods() {
+	//ClassDB::bind_method(D_METHOD("skeleton_set_bone_pose_positions", "skeleton", "bones", "positions"), &Context::skeleton_set_bone_pose_positions);
+	//ClassDB::bind_method(D_METHOD("skeleton_set_bone_pose_rotations", "skeleton", "bones", "rotations"), &Context::skeleton_set_bone_pose_rotations);
+	//ClassDB::bind_method(D_METHOD("skeleton_set_bone_pose_scales", "skeleton", "bones", "scales"), &Context::skeleton_set_bone_pose_scales);
+}
