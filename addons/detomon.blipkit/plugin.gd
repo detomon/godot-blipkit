cd2b7abb24ed9e62dfcd23b5a1d145c1e9a608b7
detomon <simon@monoxid.ch>
2025-01-11 11:32:35 +0100

    Remove editors for now

--- addons/detomon.blipkit/plugin.gd
+++ addons/detomon.blipkit/plugin.gd
@@ -1,54 +1,10 @@
 @tool
 extends EditorPlugin
 
-const WAVEFORM_EDITOR := preload("editors/waveform/waveform_panel.tscn")
-
-const ResourceEditor := preload("editors/resource_editor.gd")
-
-var _resource_editors: Array[ResourceEditor] = [
-	WAVEFORM_EDITOR.instantiate(),
-]
-
 
 func _enter_tree() -> void:
-	var undo_redo := get_undo_redo()
-
-	for editor in _resource_editors:
-		editor.undo_redo = undo_redo
-
-		var title := editor.get_panel_title()
-		var button := add_control_to_bottom_panel(editor, title)
-
-		editor.panel_button = button
-		button.visible = false
+	pass
 
 
 func _exit_tree() -> void:
-	for editor in _resource_editors:
-		remove_control_from_bottom_panel(editor)
-
-
-func _handles(object: Object) -> bool:
-	for editor in _resource_editors:
-		if editor._handles(object):
-			return true
-
-	return false
-
-
-func _edit(object: Object) -> void:
-	var handler: ResourceEditor
-
-	for editor in _resource_editors:
-		if editor._handles(object):
-			handler = editor
-			break
-
-	for editor in _resource_editors:
-		editor.panel_button.visible = editor == handler
-
-	if handler:
-		make_bottom_panel_item_visible(handler)
-		handler._edit(object)
-	else:
-		hide_bottom_panel()
+	pass
