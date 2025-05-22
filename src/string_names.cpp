3b8c2f6fa03a103903cb380130f34058a09335d2
detomon <simon@monoxid.ch>
2025-05-22 11:59:07 +0200

    Update string names

--- src/string_names.cpp
+++ src/string_names.cpp
@@ -16,20 +16,3 @@ void StringNames::free() {
 		singleton = nullptr;
 	}
 }
-
-StringNames::StringNames() {
-	_bytes = "_bytes";
-	_frames = "_frames";
-	BlipKitBytecode = "BlipKitBytecode";
-	delta = "delta";
-	envelope_ = "envelope";
-	envelope_duty_cycle = "envelope/duty_cycle";
-	envelope_panning = "envelope/panning";
-	envelope_pitch = "envelope/pitch";
-	envelope_volume = "envelope/volume";
-	repeat_mode = "repeat_mode";
-	slide_ticks = "slide_ticks";
-	sustain_end = "sustain_end";
-	sustain_offset = "sustain_offset";
-	ticks = "ticks";
-}
--- src/string_names.hpp
+++ src/string_names.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/variant/string_name.hpp>
 
 using namespace godot;
@@ -12,27 +11,25 @@ private:
 	static inline StringNames *singleton = nullptr;
 
 public:
-	StringName _bytes;
-	StringName _frames;
-	StringName BlipKitBytecode;
-	StringName delta;
-	StringName envelope_;
-	StringName envelope_duty_cycle;
-	StringName envelope_panning;
-	StringName envelope_pitch;
-	StringName envelope_volume;
-	StringName repeat_mode;
-	StringName slide_ticks;
-	StringName sustain_end;
-	StringName sustain_offset;
-	StringName ticks;
+	StringName _bytes = "_bytes";
+	StringName _frames = "_frames";
+	StringName BlipKitBytecode = "BlipKitBytecode";
+	StringName delta = "delta";
+	StringName envelope_ = "envelope";
+	StringName envelope_duty_cycle = "envelope/duty_cycle";
+	StringName envelope_panning = "envelope/panning";
+	StringName envelope_pitch = "envelope/pitch";
+	StringName envelope_volume = "envelope/volume";
+	StringName repeat_mode = "repeat_mode";
+	StringName slide_ticks = "slide_ticks";
+	StringName sustain_end = "sustain_end";
+	StringName sustain_offset = "sustain_offset";
+	StringName ticks = "ticks";
 
 	static void create();
 	static void free();
-	static _ALWAYS_INLINE_ const StringNames *get_singleton() { return singleton; }
 
-	StringNames();
-	~StringNames() = default;
+	static _ALWAYS_INLINE_ const StringNames *get_singleton() { return singleton; }
 };
 
 } //namespace BlipKit
